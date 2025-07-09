29cc0f6bf14919ba96990f2aaaa776d1a5442b5a
Jongwon Han <76779538+Hanjongwon@users.noreply.github.com>
2025-07-09 22:41:16 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
+    vector<string> answer;
+    
+    for (int i = 0; i < n; i++) {
+        int combined = arr1[i] | arr2[i];
+        string row;
+        
+        for (int j = n-1; j>=0; j--) {
+            if (combined & (1 << j)) row += '#';
+            else row += " ";
+        }
+        answer.push_back(row);
+    }
+    return answer;
+}
