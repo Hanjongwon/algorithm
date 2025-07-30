e1838e8b86128b6fc48e5f48622d6092a7c2ffed
Jongwon Han <76779538+Hanjongwon@users.noreply.github.com>
2025-07-30 22:11:36 +0900

    [Silver V] Title: 집합, Time: 596 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -1,62 +1,34 @@
-#include <algorithm>
 #include <iostream>
-#include <vector>
+#include <string>
 using namespace std;
 
-int m, n;
-vector<int> v;
-string s;
-
-void solve1(string cp, int num) {
-    auto it = find(v.begin(), v.end(), num);
-
-    if (cp == "add") {
-        if (it != v.end()) return;  // v에 num이 있다면
-        v.push_back(num);
-    }
-    if (cp == "remove") {
-        if (it == v.end()) return;  // v에 num이 없다면
-        v.erase(it);
-    }
-    if (cp == "check") {
-        if (it != v.end()) {
-            cout << 1 << '\n';
-            return;
-        } else {
-            cout << 0 << '\n';
-            return;
-        }
-    }
-    if (cp == "toggle") {
-        if (it == v.end()) {
-            v.push_back(num);
-        } else {
-            v.erase(it);
-        }
-    }
-}
-
-void solve2(string& cp) {
-    if (cp == "all") {
-        v.clear();
-        for (int i = 1; i <= 20; i++) v.push_back(i);
-    }
-    if (cp == "empty") {
-        v.clear();
-    }
-}
+bool s[21]; // 1~20 인덱스 사용
+int m, x;
+string cmd;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
     cin >> m;
-    for (int i = 0; i < m; i++) {
-        cin >> s;
-        if (s != "all" && s != "empty") {
-            cin >> n;
-            solve1(s, n);
-        } else
-            solve2(s);
+    while (m--) {
+        cin >> cmd;
+        if (cmd == "add") {
+            cin >> x;
+            s[x] = true;
+        } else if (cmd == "remove") {
+            cin >> x;
+            s[x] = false;
+        } else if (cmd == "check") {
+            cin >> x;
+            cout << s[x] << '\n';
+        } else if (cmd == "toggle") {
+            cin >> x;
+            s[x] = !s[x];
+        } else if (cmd == "all") {
+            for (int i = 1; i <= 20; i++) s[i] = true;
+        } else if (cmd == "empty") {
+            for (int i = 1; i <= 20; i++) s[i] = false;
+        }
     }
 }
