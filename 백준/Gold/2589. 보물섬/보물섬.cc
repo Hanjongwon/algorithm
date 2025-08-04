fff9b8412e0c6d96943a5e592502901ff9ae73a6
Jongwon Han <76779538+Hanjongwon@users.noreply.github.com>
2025-08-04 21:54:15 +0900

    [Gold V] Title: 보물섬, Time: 116 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.cc
+++ 백준/Gold/2589. 보물섬/보물섬.cc
@@ -1,15 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, ret, visited[50][50];
-const int dy[] = {-1, 0, 1, 0};
-const int dx[] = {0, 1, 0, -1};
-char a[50][50];
 
-void bfs(int y, int x) {
+int n, m, visited[50][50], ret = -987654321;  // n: 세로, m : 가로
+char arr[50][50];
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1};
+
+int bfs(int y, int x) {
     memset(visited, 0, sizeof(visited));
     visited[y][x] = 1;
     queue<pair<int, int>> q;
     q.push({y, x});
+    int cnt = 0;
 
     while (q.size()) {
         tie(y, x) = q.front();
@@ -18,16 +20,19 @@ void bfs(int y, int x) {
         for (int i = 0; i < 4; i++) {
             int ny = y + dy[i];
             int nx = x + dx[i];
-            if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+
+            if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
             if (visited[ny][nx]) continue;
-            if (a[ny][nx] == 'W') continue;
+            if (arr[ny][nx] == 'W') continue;
 
             visited[ny][nx] = visited[y][x] + 1;
+            cnt             = max(cnt, visited[ny][nx]);
             q.push({ny, nx});
-            ret = max(ret, visited[ny][nx]);
         }
     }
+    return cnt;
 }
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -35,13 +40,16 @@ int main() {
     cin >> n >> m;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            cin >> a[i][j];
+            cin >> arr[i][j];
         }
     }
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            if (a[i][j] == 'L') bfs(i, j);
+            if (arr[i][j] == 'L') {
+                int cnt = bfs(i, j);
+                ret     = max(cnt, ret);
+            }
         }
     }
     cout << ret - 1;
