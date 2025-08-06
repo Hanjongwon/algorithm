aeca818ca443648172d2a7c02f966bb1691eb30e
Jongwon Han <76779538+Hanjongwon@users.noreply.github.com>
2025-08-06 14:55:32 +0900

    [level 2] Title: 전화번호 목록, Time: 88.03 ms, Memory: 35.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool ret;
+
+bool solution(vector<string> phone_book) {
+    sort(phone_book.begin(), phone_book.end());
+    for (int i = 0; i < phone_book.size()-1; i++) {
+        string now = phone_book[i];
+        string next = phone_book[i+1];
+        if (next.find(now) == 0) return false; 
+    }
+    return true;
+}
