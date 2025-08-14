15ee0c6d6deb67062fb8e9ee67caa00da9f4e3fc
Jongwon Han <76779538+Hanjongwon@users.noreply.github.com>
2025-08-14 20:18:22 +0900

    [level 2] Title: 시소 짝꿍, Time: 22.10 ms, Memory: 6.64 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.cpp
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.cpp
@@ -1,24 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
+long long ret;
+unordered_map<int, int> mp;
+vector<pair<int, int>> ratios = {{1,1}, {2,3}, {3,2}, {2,4}, {3,4}, {4,2}, {4,3}};
 
 long long solution(vector<int> weights) {
-    unordered_map<int, long long> cnt;   // 과거에 본 몸무게 → 빈도
-    long long ans = 0;
-
-    // 가능한 좌석 거리 비율 (분자, 분모). 양방향을 모두 포함
-    const pair<int,int> ratios[] = {
-        {1,1}, {2,3}, {3,2}, {2,4}, {4,2}, {3,4}, {4,3}
-    };
-
-    for (int w : weights) {
-        for (auto [num, den] : ratios) {
-            long long lhs = 1LL * w * den;         // w * 분모
-            if (lhs % num != 0) continue;          // 정수로 딱 떨어질 때만
-            int target = (int)(lhs / num);         // 맞춰야 하는 상대 몸무게
-            auto it = cnt.find(target);
-            if (it != cnt.end()) ans += it->second;
+    for (int i : weights) {
+        for (auto p : ratios) {
+           	long long lhs = 1LL * i * p.second;
+            if (lhs % p.first) continue;
+            int check = lhs / p.first;
+            if (mp.count(check) != 0) {
+                ret += mp[check];
+            }
         }
-        cnt[w]++;                                   // 현재 몸무게를 ‘과거’로 등록
+        mp[i]++;
     }
-    return ans;
+    return ret;
 }
